c9dd29dc0e042db38193862e7d2acbbfeabf6eee
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-03-19 01:55:04 +0500

    Add files via upload
    I've completed 6th Assighment 🎉 Governer Sindh IT Intiative Kamran Khan Tessori

---
+++ Q6.js
@@ -0,0 +1,4 @@
+var personName = "\n\t Ubaidalishaikh \t\n";
+console.log(personName);
+var stripping = personName.trim();
+console.log(stripping);
---
+++ Q6.ts
@@ -0,0 +1,5 @@
+let personName: string = "\n\t Ubaidalishaikh \t\n";
+console.log(personName);
+
+let stripping = personName.trim();
+console.log(stripping);
